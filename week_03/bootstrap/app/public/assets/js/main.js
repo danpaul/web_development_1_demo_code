e8d007db7a1db437859a294325b569f14e570705
Dan Breczinski <pt2323@gmail.com>
2024-11-24 15:49:07 +0100

    better error handling in js

--- week_03/bootstrap/app/public/assets/js/main.js
+++ week_03/bootstrap/app/public/assets/js/main.js
@@ -2,11 +2,15 @@ window.addEventListener("load", (event) => {
   const myModal = document.getElementById("exampleModal");
   const saveButton = document.getElementById("save-button");
 
-  myModal.addEventListener("shown.bs.modal", () => {
-    console.log("model opened");
-  });
+  if (myModal) {
+    myModal.addEventListener("shown.bs.modal", () => {
+      console.log("model opened");
+    });
+  }
 
-  saveButton.addEventListener("click", () => {
-    console.log("save button clicked");
-  });
+  if (saveButton) {
+    saveButton.addEventListener("click", () => {
+      console.log("save button clicked");
+    });
+  }
 });
